51900fe4243ed9e5939653e37bdd03235240e740
tsolmon <tsolmon_mm@yahoo.com>
2019-07-07 13:30:48 -0400

    deleted json plugin empty space

--- src/test/Runner/FirstPageRunner.java
+++ src/test/Runner/FirstPageRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin={"html:target/Reports","rerun:target/rerun.txt","json: target/FirstPageRunner.json"},
+        plugin={"html:target/Reports","rerun:target/rerun.txt","json:target/FirstPageRunner.json"},
         features = "/Users/Tsolmon/IdeaProjects/HakuProject/src/main/resources/FirstPage.feature:96"
         ,glue = {"step_definitions"}
         //,tags= {"@test"}
