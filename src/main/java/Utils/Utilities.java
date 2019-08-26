d6ab96bf8a156ac3af886bd7aa1d2ad01ec4d9be
tsolmon <tsolmon_mm@yahoo.com>
2019-08-25 20:06:19 -0400

    deleted json plugin empty space

--- src/main/java/Utils/Utilities.java
+++ src/main/java/Utils/Utilities.java
@@ -1,10 +1,18 @@
 package Utils;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.*;
 import step_definitions.Hooks;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class Utilities {
@@ -41,8 +49,73 @@ public class Utilities {
        return p.getProperty(key);
     }
 
+    public static ArrayList<String> excelReader(String fullPath, String rowName){
 
 
+        ArrayList<String> list = new ArrayList<String>();
+
+        try {
+
+            FileInputStream stream = new FileInputStream(fullPath);
+            XSSFWorkbook workbook = new XSSFWorkbook(stream);
+            XSSFSheet sheet = workbook.getSheet("Sheet1");
+
+
+
+            Row row=null;
+
+            for(int i=0; i<sheet.getLastRowNum(); i++){
+                row=sheet.getRow(i);
+                if(row.getCell(0).toString().equalsIgnoreCase(rowName)){
+                    for(int k=0; k<row.getLastCellNum(); k++){
+                        list.add(row.getCell(k).toString());
+                    }
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+           return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(excelReader("/Users/Tsolmon/Desktop/GetData.xlsx","Talgat"));
+    }
+
+
+    public static void getData(){
+        WebDriver driver=BrowserUtils.getDriver();
+
+        ArrayList<String> lastname= new ArrayList<>();
+        lastname.add("BOUTHILLER");
+        lastname.add("SIMS");
+        lastname.add("NICK");
+        lastname.add("RUDNISKY");
+        lastname.add("RICCIO");
+        lastname.add("CROSS");
+        ArrayList<String> ssn= new ArrayList<>();
+        ssn.add("666017001");
+        ssn.add("666189411");
+        ssn.add("666193191");
+        ssn.add("666566504");
+        ssn.add("666345680");
+        ssn.add("666967842");
+     for(int i=0; i<lastname.size(); i++){
+         driver.findElement(By.xpath("xpath of your lastName")).sendKeys(lastname.get(i));
+         driver.findElement(By.xpath("xpath of your SSN")).sendKeys(ssn.get(i));
+         driver.findElement(By.xpath("delete button xpath")).click();
+     }
+
+
+    }
 
 
 
